cd13fca078d06114416603460d07160652737a68
treezyxd <thalesmaltaxd@gmail.com>
2023-04-22 16:10:54 -0300

    connection mongodb done

--- config/database.js
+++ config/database.js
@@ -2,3 +2,14 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
+let db;
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+
+try {
+  await mongoClient.connect();
+  db = mongoClient.db();
+} catch(err) {
+  console.log(err);
+}
+
+export default db;
