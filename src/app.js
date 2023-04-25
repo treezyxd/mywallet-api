efec02c058024e6389a005523eca31b8f26b5a76
treezyxd <thalesmaltaxd@gmail.com>
2023-04-25 12:53:08 -0300

    alter port changes

--- src/app.js
+++ src/app.js
@@ -13,4 +13,6 @@ app.use(cors());
 app.use(actionRouter);
 app.use(authRouter);
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
